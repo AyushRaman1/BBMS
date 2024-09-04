e48de9ef059e914c115a03664a4bfcf6db6c5c2b
AyushRaman1 <118748868+AyushRaman1@users.noreply.github.com>
2024-09-04 12:45:10 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,4 @@
+•	Developed an advanced Python program* for dynamic hand gesture recognition, offering precise tracking of individual fingers, real-time feedback, and FPS monitoring.
+•	Enhanced human-computer interaction* by enabling nuanced recognition of finger movements, including the detection of clockwise and counterclockwise motion.
+•	Implemented cutting-edge computer vision algorithms* and libraries to ensure high performance and responsiveness in various applications such as virtual reality, gaming, and educational tools.
+
